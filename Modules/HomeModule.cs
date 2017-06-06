ae384f775bff589f64f4b2b463c692eff6c48dc4
guy <luckythelich@hotmail.com>
2017-06-06 07:57:34 -0700

    blah

--- Modules/HomeModule.cs
+++ Modules/HomeModule.cs
@@ -1,37 +1,15 @@
 using Nancy;
 using System.Collections.Generic;
-using Inventory.Objects;
+using !!!!
 using System;
 
-namespace Inventory
+namespace !!!!!!
 {
   public class HomeModule : NancyModule
   {
     public HomeModule()
     {
-      Get["/"] = _ => {
-        return View["index.cshtml"];
-      };
 
-      Get["/inventory"] = _ => {
-        List<MyLittlePony> allMyLittlePonies = MyLittlePony.GetAll();
-        return View["inventory.cshtml", allMyLittlePonies];
-      };
-
-      Post["/inventory"] = _ =>  {
-        MyLittlePony newMyLittlePony = new MyLittlePony(Request.Form["pony-name"], Request.Form["pony-color"]);
-        Console.WriteLine("before GetAll");
-        Console.WriteLine(newMyLittlePony.GetName());
-        List<MyLittlePony> allMyLittlePonies = MyLittlePony.GetAll();
-        Console.WriteLine("after GetAll");
-        Console.WriteLine(allMyLittlePonies[0].GetName());
-        return View["inventory.cshtml", allMyLittlePonies];
-      };
-
-      Get["/inventory/{id}"] = parameters => {
-        MyLittlePony myLittlePony = MyLittlePony.Find(parameters.id);
-        return View["pony.cshtml", myLittlePony];
-      };
 
 
     }
--- Tests/InventoryTest.cs
+++ Tests/InventoryTest.cs
@@ -5,71 +5,11 @@ using System.Data;
 using System.Data.SqlClient;
 using Inventory.Objects;
 
-namespace Inventory
+namespace !!!!!
 {
-  public class InventoryTest : IDisposable
+  public class !!!!Test : IDisposable
   {
-    public InventoryTest()
-    {
-      DBConfiguration.ConnectionString = "Data Source=(localdb)\\mssqllocaldb;Initial Catalog=inventory_test;Integrated Security=SSPI;";
-    }
+    public !!!!Test()
 
-    public void Dispose()
-    {
-      MyLittlePony.DeleteAll();
-    }
-
-    [Fact]
-    public void Test_DatabaseEmptyAtFirst()
-    {
-      //Arrange, Act
-      int result = MyLittlePony.GetAll().Count;
-
-      //Assert
-      Assert.Equal(0, result);
-    }
-    [Fact]
-    public void Test_Save_SavesToDatabase()
-    {
-      //Arrange
-      MyLittlePony testMyLittlePony = new MyLittlePony("Applejack", "Orange");
-
-      //Act
-      testMyLittlePony.Save();
-      List<MyLittlePony> result = MyLittlePony.GetAll();
-      List<MyLittlePony> testList = new List<MyLittlePony>{testMyLittlePony};
-
-      //Assert
-      Assert.Equal(testList, result);
-    }
-    [Fact]
-    public void Test_Save_AssignsIdToObject()
-    {
-      //Arrange
-      MyLittlePony testMyLittlePony = new MyLittlePony("Applejack", "Orange");
-
-      //Act
-      testMyLittlePony.Save();
-      MyLittlePony savedMyLittlePony = MyLittlePony.GetAll()[0];
-
-      int result = savedMyLittlePony.GetId();
-      int testId = testMyLittlePony.GetId();
-
-      //Assert
-      Assert.Equal(testId, result);
-    }
-    [Fact]
-    public void Test_Find_FindsMyLittlePonyInDatabase()
-    {
-      //Arrange
-      MyLittlePony testMyLittlePony = new MyLittlePony("Applejack", "Orange");
-      testMyLittlePony.Save();
-
-      //Act
-      MyLittlePony foundMyLittlePony = MyLittlePony.Find(testMyLittlePony.GetId());
-
-      //Assert
-      Assert.Equal(testMyLittlePony, foundMyLittlePony);
-    }
   }
 }
