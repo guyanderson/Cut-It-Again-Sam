2da22cc68b0c5682d6c2fac8f6d0187e4fa311ec
guy <luckythelich@hotmail.com>
2017-06-09 09:57:58 -0700

    Add equals override and save methods. Did not fix DB does not exist error as hoped.

--- Objects/Stylist.cs
+++ Objects/Stylist.cs
@@ -71,5 +71,48 @@ namespace Stylist_Object
       conn.Close();
     }
 //============================================
+public override bool Equals(System.Object otherStylist)
+{
+  if (!(otherStylist is Stylist))
+  {
+    return false;
+  }
+  else
+  {
+    Stylist newStylist = (Stylist) otherStylist;
+    bool idEquality = (this.GetId() == newStylist.GetId());
+    bool nameEquality = (this.GetName() == newStylist.GetName());
+    return (nameEquality);
+  }
+}
+//============================================
+public void Save()
+{
+  SqlConnection conn = DB.Connection();
+  conn.Open();
+
+  SqlCommand cmd = new SqlCommand("INSERT INTO stylists (name) OUTPUT INSERTED.id VALUES (@StylistName)", conn);
+
+  SqlParameter nameParameter = new SqlParameter();
+  nameParameter.ParameterName = "@StylistName";
+  nameParameter.Value = this.GetName();
+  cmd.Parameters.Add(nameParameter);
+
+  SqlDataReader rdr = cmd.ExecuteReader();
+
+  while(rdr.Read())
+  {
+    this._id = rdr.GetInt32(0);
+  }
+  if (rdr != null)
+  {
+    rdr.Close();
+  }
+  if (conn != null)
+  {
+    conn.Close();
+  }
+}
+//============================================
   }
 }
