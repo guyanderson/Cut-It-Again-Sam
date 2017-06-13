ce685cf85144d864f410454542d47714a920f796
guyanderson <luckythelich@hotmail.com>
2017-06-13 08:50:55 -0700

    change file name

---
+++ hair_salon_script.sql
@@ -0,0 +1,33 @@
+﻿USE [hair_salon]
+GO
+/****** Object:  Table [dbo].[clients]    Script Date: 6/10/2017 4:01:52 PM ******/
+SET ANSI_NULLS ON
+GO
+SET QUOTED_IDENTIFIER ON
+GO
+CREATE TABLE [dbo].[clients](
+	[id] [int] IDENTITY(1,1) NOT NULL,
+	[name] [varchar](255) NULL,
+	[stylist_id] [int] NULL
+) ON [PRIMARY]
+
+GO
+/****** Object:  Table [dbo].[stylists]    Script Date: 6/10/2017 4:01:52 PM ******/
+SET ANSI_NULLS ON
+GO
+SET QUOTED_IDENTIFIER ON
+GO
+CREATE TABLE [dbo].[stylists](
+	[name] [varchar](255) NULL,
+	[id] [int] IDENTITY(1,1) NOT NULL
+) ON [PRIMARY]
+
+GO
+SET IDENTITY_INSERT [dbo].[clients] ON 
+
+INSERT [dbo].[clients] ([id], [name], [stylist_id]) VALUES (1, N'Bob', 1)
+SET IDENTITY_INSERT [dbo].[clients] OFF
+SET IDENTITY_INSERT [dbo].[stylists] ON 
+
+INSERT [dbo].[stylists] ([name], [id]) VALUES (N'Sam', 1)
+SET IDENTITY_INSERT [dbo].[stylists] OFF
