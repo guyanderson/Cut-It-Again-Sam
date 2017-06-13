bd2d384a4dbeaadc028db641dfc3696d6047f0a7
guyanderson <luckythelich@hotmail.com>
2017-06-13 08:49:59 -0700

    change file name

---
+++ hair_salon_test.sql
@@ -0,0 +1,25 @@
+﻿USE [hair_salon_test]
+GO
+/****** Object:  Table [dbo].[clients]    Script Date: 6/10/2017 12:15:16 PM ******/
+SET ANSI_NULLS ON
+GO
+SET QUOTED_IDENTIFIER ON
+GO
+CREATE TABLE [dbo].[clients](
+	[id] [int] IDENTITY(1,1) NOT NULL,
+	[name] [varchar](255) NULL,
+	[stylist_id] [int] NULL
+) ON [PRIMARY]
+
+GO
+/****** Object:  Table [dbo].[stylists]    Script Date: 6/10/2017 12:15:16 PM ******/
+SET ANSI_NULLS ON
+GO
+SET QUOTED_IDENTIFIER ON
+GO
+CREATE TABLE [dbo].[stylists](
+	[name] [varchar](255) NULL,
+	[id] [int] IDENTITY(1,1) NOT NULL
+) ON [PRIMARY]
+
+GO
